5bce74f1ce7ea7f25bfa2c2fda17e439b3f69433
細見元気 <hosomi2001@gmail.com>
2024-12-16 00:32:44 +0900

    useModeを導入し、modeを処理する。

--- src/component/pages/HomePage.tsx
+++ src/component/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 // ホームのpagesを作成
 import React from 'react';
+import useMode from '../../hooks/useMode';
 import useSelectedPrefectures from '../../hooks/useSelectedPrefectures';
 import { useGetPopulation } from '../../services/api/population/useGetPopulation';
 import { useGetPrefectures } from '../../services/api/prefecture/useGetPrefecture';
@@ -9,6 +10,9 @@ const HomePage: React.FC = () => {
   // チェックボックスに選択されている都道府県を設定
   const { prefectures: selectedPrefectures, handleSelectedPrefectures } =
     useSelectedPrefectures();
+  // モードを選択
+  const { mode, handleChangeMode } = useMode();
+
   // 都道府県データと人口データを取得
   const { data: prefectures } = useGetPrefectures();
   const { data: population } = useGetPopulation(selectedPrefectures);
@@ -18,6 +22,8 @@ const HomePage: React.FC = () => {
       population={population ?? []}
       selectedPrefectures={selectedPrefectures}
       handleSelectedPrefectures={handleSelectedPrefectures}
+      mode={mode}
+      handleChangeMode={handleChangeMode}
     />
   );
 };
