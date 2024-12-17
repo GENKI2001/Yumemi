0e317441612b1e11aaa843fbd621c2f41f49352f
細見元気 <hosomi2001@gmail.com>
2024-12-17 22:49:27 +0900

    不要なコメントアウトを削除。

--- src/component/pages/HomePage.tsx
+++ src/component/pages/HomePage.tsx
@@ -7,15 +7,12 @@ import useSelectedPrefectures from '../../hooks/useSelectedPrefectures';
 import HomeTemplate from '../templates/HomeTemplate';
 
 const HomePage: React.FC = () => {
-  // チェックボックスに選択されている都道府県を設定
   const { prefectures: selectedPrefectures, handleSelectedPrefectures } =
     useSelectedPrefectures([{ prefCode: 13, prefName: '東京都' }]);
-  // モードを選択
   const { mode, handleChangeMode } = useMode();
-
-  // 都道府県データと人口データを取得
   const { data: prefectures } = useGetPrefectures();
   const { data: population } = useGetPopulation(selectedPrefectures);
+
   return (
     <HomeTemplate
       prefectures={prefectures ?? []}
