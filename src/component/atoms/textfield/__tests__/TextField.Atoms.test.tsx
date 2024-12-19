6c1d747c73c8d564f70380271c19a5e1b7752068
細見元気 <hosomi2001@gmail.com>
2024-12-19 18:40:02 +0900

    atoms層に汎用的なテキストフィールドを実装。

---
+++ src/component/atoms/textfield/__tests__/TextField.Atoms.test.tsx
@@ -0,0 +1,76 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import TextFieldAtoms from '../TextField.Atoms';
+
+describe('TextFieldAtoms', () => {
+  it('renders the label correctly', () => {
+    render(
+      <TextFieldAtoms
+        label="Username"
+        value=""
+        onChange={() => {}}
+        placeholder="Enter your username"
+      />,
+    );
+    const labelElement = screen.getByText('Username');
+    expect(labelElement).toBeInTheDocument();
+  });
+
+  it('renders the input field with placeholder', () => {
+    render(
+      <TextFieldAtoms
+        label="Username"
+        value=""
+        onChange={() => {}}
+        placeholder="Enter your username"
+      />,
+    );
+    const inputElement = screen.getByPlaceholderText('Enter your username');
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  it('displays an error message if provided', () => {
+    render(
+      <TextFieldAtoms
+        label="Username"
+        value=""
+        onChange={() => {}}
+        error="This field is required"
+      />,
+    );
+    const errorMessage = screen.getByText('This field is required');
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it('calls onChange when the input value changes', () => {
+    const handleChange = jest.fn();
+    render(
+      <TextFieldAtoms
+        label="Username"
+        value=""
+        onChange={handleChange}
+        placeholder="Enter your username"
+      />,
+    );
+
+    const inputElement = screen.getByPlaceholderText(
+      'Enter your username',
+    ) as HTMLInputElement;
+    fireEvent.change(inputElement, { target: { value: 'John' } });
+
+    expect(handleChange).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders the input field with the correct type', () => {
+    render(
+      <TextFieldAtoms
+        label="Password"
+        value=""
+        onChange={() => {}}
+        placeholder="Enter your password"
+        type="password"
+      />,
+    );
+    const inputElement = screen.getByPlaceholderText('Enter your password');
+    expect(inputElement).toHaveAttribute('type', 'password');
+  });
+});
