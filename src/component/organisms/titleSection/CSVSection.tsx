0c41edb8cae013f60ec10041ca6e139d760cd3b6
細見元気 <hosomi2001@gmail.com>
2024-12-20 15:50:45 +0900

    CSV出力のセクションを追加、表示。

---
+++ src/component/organisms/titleSection/CSVSection.tsx
@@ -0,0 +1,12 @@
+import { TitleDescription } from '../../molecules/text/TitleDescription';
+
+const CSVSection = () => {
+  return (
+    <TitleDescription
+      title={'CSV出力'}
+      description={'(ログイン必須) 選択中のデータをダウンロードしてみよう！！'}
+    />
+  );
+};
+
+export default CSVSection;
